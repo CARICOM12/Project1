ce0890d53f0e7ebdc557db62ee6cd87f1e5592d4
CARICOM12 <0190a@ukr.net>
2017-01-29 23:27:20 +0200

    Adding city class and hello world sample

---
+++ Project1/Project1/City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class City
+    {
+        public string Name { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
